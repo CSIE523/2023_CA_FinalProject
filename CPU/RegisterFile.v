c88dc84b5a3b0fa3e5747a99ac1751e7f876efab
Hsiang0528 <ken90528@gmail.com>
2024-01-05 15:40:15 +0800

    Fix bug

--- CPU/RegisterFile.v
+++ CPU/RegisterFile.v
@@ -1,8 +1,8 @@
-module REG(write_enable, write_address, write_data, read_address1, read_address2, read_data1, read_data2);
+module REG(rst,write_enable, write_address, write_data, read_address1, read_address2, read_data1, read_data2);
 parameter PhysicalRegisterAddrWidth = 5;
 parameter DataWidth = 32;
 
-input write_enable;
+input rst,write_enable;
 input [PhysicalRegisterAddrWidth-1:0]write_address;
 input [DataWidth-1:0]write_data;
 
@@ -26,6 +26,12 @@ reg [DataWidth-1:0]registers[0:PhysicalRegisterAddrWidth-1];
 //     end
 //     else begin
 always @(*) begin
+    if(rst)begin
+        for (i = 0;i < 32;i = i + 1) begin
+            registers[i] <= 0;
+        end
+    end
+    else begin
         if(write_enable && write_address != 0)begin
             registers[write_address] <= write_data;
         end
@@ -39,12 +45,12 @@ always @(*) begin
             read_data2 <= 0;
         else 
             read_data2 <= registers[read_address2];
-
         // if(debug_read_address == 0)
         //     debug_read_data <= 0;
         // else 
         //     debug_read_data <= registers[debug_read_address];
     end
+end
 // end
 
 
